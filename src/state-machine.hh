27958c4f8ec5b0381a381e8f225e2b9f00aebd61
Derek <physci@gmail.com>
2018-12-07 09:40:18 -0500

    UI state handling may be better handled in lua with a msg queue.
    Instead of wrapping all the c++ objects, add a message queue lua
    pushes onto.

--- src/state-machine.hh
+++ src/state-machine.hh
@@ -2,8 +2,18 @@
 
 #include <vector>
 #include <iostream>
+#include <typeinfo>
+
+#include "err.hh"
 
 using namespace std;
+
+// why not do this in lua!
+// 
+
+// template <class T>
+// string show(const T &obj);
+
 
 template <class S, class T>
 class Transition {
@@ -12,10 +22,21 @@ private:
   T m_trigger;
   S m_tgt;
 
-public:
+public: 
   Transition(T trigger, S src, S tgt) :
     m_src(src), m_trigger(trigger), m_tgt(tgt) {}
   
+  const S getTarget() const {
+    return m_tgt;
+  }
+
+  const T getTrigger() const {
+    return m_trigger;
+  }
+
+  const S getSource() const {
+    return m_src;
+  }
 };
 
 template <class S, class T>
@@ -26,10 +47,10 @@ private:
 
 public:
   StateMachine<S, T>() { }
+  
   ~StateMachine<S, T>() {
     cerr << "Destroying state machine." << std::endl;
   }
-
   
   void addTransition(T trigger, S src, S tgt) {
     Transition<S, T> t(trigger, src, tgt);
@@ -39,6 +60,17 @@ public:
   void startAt(S startState) {
     m_curState = startState;
   }
+
+  void fire(T trigger) {
+    for (auto t : m_transitions) {
+      if (t.getSource() == m_curState && t.getTrigger == trigger) {
+        m_curState = t.getTarget();
+        return;
+      }
+    }
+    die("Could not fire state machine, transition not found for current state");
+  }
+
 };
 
 
