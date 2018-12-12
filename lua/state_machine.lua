ef980d0b89b53d951585ee825c4e2eb549a1aff4
Derek <physci@gmail.com>
2018-12-11 21:10:08 -0500

    probably not going to use lua for state machines.

--- lua/state_machine.lua
+++ lua/state_machine.lua
@@ -1,40 +1,64 @@
-function getArgs(fun)
-local args = {}
-local hook = debug.gethook()
 
-local argHook = function( ... )
-    local info = debug.getinfo(3)
-    if 'pcall' ~= info.name then return end
 
-    for i = 1, math.huge do
-        local name, value = debug.getlocal(2, i)
-        if '(*temporary)' == name then
-            debug.sethook(hook)
-            error('')
-            return
-        end
-        table.insert(args,name)
-    end
-end
 
-debug.sethook(argHook, "c")
-pcall(fun)
 
-return args
-end
-
-function StateMachine()
+function StateMachine(name)
    self = {}
+   self.name = name   
+   self.currentState = nil
+   self.transitions = {}
    
-   self.StateA = function() end
-   self.StateB = function() end
+   -------------------------------------------------------------------------------------------------
+   self.add = function(source, trigger, target, action)
+      if not self.transitions[source] then
+         self.transitions[source] = {}
+      end
+      
+      if self.transitions[source][trigger] then         
+         local badTransition = "[" .. source .. " >" .. trigger .. "> " .. target "]"
+         local msg = "StateMachine.add trying to replace a transition: " .. badTransition
+         return {err = true, msg = msg}
+      else
+         self.transitions[source][trigger] = target
+         return {err = false, msg=""}
+      end
+   end
 
+
+   self.graphviz = function()      
+      print ("digraph " .. self.name .. " {")
+      for src, srcTable in pairs(self.transitions) do
+         if srcTable then
+            for trans, target in pairs(srcTable) do
+               print (src .. " -> " .. target .. ' [label="' .. trans .. '"];')
+            end
+         end
+      end
+      print ("}")
+
+   end
+   
    return self
 end
 
+
 function main()
-   local function Color(Red, Blue, Gree) end
-   print(getArgs(Color))
+   local sm = StateMachine("Door")
+
+   
+   sm.add("opened", "push", "closed")
+   sm.add("opened", "pull", "opened")
+   sm.add("closed", "pull", "opened")
+   sm.add("closed", "push", "closed")
+   
+   sm.graphviz()
+      
 end
+
+function CellMachine()
+   local sm = StateMachine("Cell")
+
+   sm.add("sleep", "MouseOver", "", )
+
 
 main()
