413981cbba6565dcea2436f3bb79b6b786c27143
Derek <physci@gmail.com>
2018-12-16 19:49:33 -0500

    adds name to event handler for better messages.

--- src/event-handler.hh
+++ src/event-handler.hh
@@ -10,9 +10,10 @@ using namespace std;
 
 class EventHandler {
 public:
+  string m_handlerName;
   std::unordered_map<SDL_EventType, std::function<void(SDL_Event&)>> m_eventTable;
     
-  EventHandler();
+  EventHandler(string name);
   void registerEventHandler(SDL_EventType et, std::function<void(SDL_Event&)>);
   void handleEvent(SDL_Event &ev);
 };
