c072b16c6ed078c1ea2001ce251b51c7932980d4
Derek <physci@gmail.com>
2019-01-17 19:35:28 -0500

    cleanup

--- src/bbox.cc
+++ src/bbox.cc
@@ -3,15 +3,6 @@
 
 #include "bbox.hh"
 
-// bool
-// bboxContains(BBox bb, int x, int y) {
-//     return ( bb.left <= x &&
-//              x <= bb.left + bb.width &&
-//              bb.top <= y &&
-//              y <= bb.top + bb.height );
-// }
-
-
 BBox::BBox() : top(0), left(0), height(0), width(0) { }
 
 BBox::BBox(int top, int left, int height, int width) :
