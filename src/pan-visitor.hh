cb43f7a5e028352ec56c484a5094e9887f4b5528
Derek <physci@gmail.com>
2019-01-18 16:58:40 -0500

    getting ready to support panning the grid.

---
+++ src/pan-visitor.hh
@@ -0,0 +1,18 @@
+#include "visit.hh"
+#include "grid-edit.hh"
+  
+class PanVisitor : public BaseVisitor 
+{
+private:
+  int m_factor;
+public:  
+  PanVisitor(string cmd) {
+    m_cmd = cmd
+  }  
+  void visit(GridEditor &ge) {
+    if (m_cmd == "pan-east") ge.panEast();
+    if (m_cmd == "pan-west") ge.panWest();
+    if (m_cmd == "pan-north") ge.panNorth();
+    if (m_cmd == "pan-south") ge.panSouth();
+  }
+};
