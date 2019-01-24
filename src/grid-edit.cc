e7fd73accd52454963f341d8115429204107a0c5
Derek <physci@gmail.com>
2019-01-24 16:23:18 -0500

    removes event handler from GridEditor

--- src/grid-edit.cc
+++ src/grid-edit.cc
@@ -5,14 +5,11 @@
 #include "grid-edit.hh"
 #include "event-handler.hh"
 
-GridEditor::GridEditor() :
-  EventHandler("GridEditor") 
-{
+GridEditor::GridEditor() {
   m_overCell = m_grid.getCell(0, 0); 
   m_selectedCell = m_grid.getCell(0, 0);
   m_hasFocus = false;
   m_statusText.setWidth(m_grid.width());
-  setupEvents();
 }
 
 GridEditor::~GridEditor() {
@@ -45,16 +42,6 @@ GridEditor::panNorth() {
 void
 GridEditor::panSouth() {
   cout << "[STUB] panning South" << endl;
-}
-
-void
-GridEditor::setupEvents() {
-  registerEventHandler(SDL_MOUSEBUTTONDOWN,
-                       [&](SDL_Event &ev) {
-                         if (!m_hasFocus) return; 
-                         m_statusText.setRow(0, "Handling SDL_MOUSEBUTTONDOWN");
-                         updateSelectedCell(ev);
-                       });
 }
 
 void
@@ -95,38 +82,6 @@ GridEditor::updateFocus(SDL_Event &ev) {
 bool
 GridEditor::hasFocus() {
   return m_hasFocus;
-}
-
-void
-GridEditor::handleTextInput(SDL_Event &ev) {
-  // this handles input for visible text chars, not the esc key, etc.
-  // there will be a state machine here the grid editor will have
-  // modes.
-  
-  if (ev.type != SDL_TEXTINPUT) die("Got the wrong event type");
-  switch(ev.window.event) {
-    
-  case 'r': {
-    // rotate heading.
-    break;
-  }
-    
-  case 'w': {
-    panNorth();
-  }
-    
-  case 'a': {
-    m_grid.setSelectAllCells(true);
-    break;
-  }
-    
-  case '?': {
-    // how to message the big text box from here?
-  }
-    
-  default: {
-    // complain about no keybinding.
-  }} // end switch
 }
 
 void
